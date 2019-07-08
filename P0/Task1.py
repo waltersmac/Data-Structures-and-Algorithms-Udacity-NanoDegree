b7fdcb4093376c160cfca84f16e3c6f028677392
Robert Walters <waltersmac@gmail.com>
2019-07-08 20:51:01 +0100

    feat: Completed Task1
    I created a function to find the number of different telephone numbers that are there in both csv files (texts, calls)
    
    Project: P0

--- P0/Task1.py
+++ P0/Task1.py
@@ -18,3 +18,29 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+# Create list for unique telephone numbers
+nums_list = []
+
+# Function for counting unique telephone numbers
+def count_nums(text_data, call_data):
+
+	# Adding numbers from texts.csv
+	for i in text_data:
+		for j in i[:-1]:
+			if j not in nums_list:
+				nums_list.append(j)
+    
+    # Adding numbers from calls.csv
+	for k in call_data:
+		for l in k[:-1]:
+			if l not in nums_list:
+				nums_list.append(l)
+    
+    # return number of unique telephone numbers
+	return len(nums_list)
+		
+
+count_all_nums = count_nums(texts, calls)			
+			
+print("There are {} different telephone numbers in the records.".format(count_all_nums))
