7247fe9f6796edfd1b7018aa948d8f6683dbb030
Robert Walters <waltersmac@gmail.com>
2019-07-14 19:30:31 +0100

    feat: Adding Problem1 to project
    Uploading new files
    
    Project: P1

---
+++ P1/problem_1.py
@@ -0,0 +1,20 @@
+class LRU_Cache(object):
+
+    def __init__(self, capacity):
+        # Initialize class variables
+        pass
+
+    def get(self, key):
+        # Retrieve item from provided key. Return -1 if nonexistent. 
+        pass
+
+    def set(self, key, value):
+        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
+        pass
+
+our_cache = LRU_Cache(5)
+
+our_cache.set(1, 1);
+our_cache.set(2, 2);
+our_cache.set(3, 3);
+our_cache.set(4, 4);
