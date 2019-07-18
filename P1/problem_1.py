5f55ff782a6dcc7879f9512ec9affd00e07b55f4
Robert Walters <waltersmac@gmail.com>
2019-07-18 21:26:53 +0100

    Adjusting intial files for promlem_1

--- P1/problem_1.py
+++ P1/problem_1.py
@@ -8,11 +8,12 @@ class LRU_Cache(object):
 
     def get(self, key):
         # Retrieve item from provided key. Return -1 if nonexistent. 
-        pass
+        
 
     def set(self, key, value):
         # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
-        pass
+        
+        
 
 our_cache = LRU_Cache(5)
 
