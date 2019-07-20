2528bc743b298aef75cdc753ba67fdd3fd63804d
Robert Walters <waltersmac@gmail.com>
2019-07-20 22:21:36 +0100

    feat: Updated problem_1
    Updated initial class varibles, and also added get() and set() methods
    
    Project: P1

--- P1/problem_1.py
+++ P1/problem_1.py
@@ -2,16 +2,24 @@ class LRU_Cache(object):
 
     def __init__(self, capacity):
         # Initialize class variables
+        self.cache = {}
+        self.key = 0
+        self.value = 0
         self.capacity = capacity
-        self.hit = None
-        self.miss = None
 
     def get(self, key):
         # Retrieve item from provided key. Return -1 if nonexistent. 
-        
+        if key < self.capacity:
+        	return key
+        return -1
 
     def set(self, key, value):
-        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
+        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
+        if key < self.capacity:
+        	self.cache[key] = value
+        else:
+        	self.cache[self.capacity] = value
+
         
         
 
@@ -21,3 +29,13 @@ our_cache.set(1, 1);
 our_cache.set(2, 2);
 our_cache.set(3, 3);
 our_cache.set(4, 4);
+
+
+print(our_cache.get(1))       # returns 1
+print(our_cache.get(2))       # returns 2
+print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
+
+our_cache.set(5, 5) 
+our_cache.set(6, 6)
+
+print(our_cache.get(3))      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
