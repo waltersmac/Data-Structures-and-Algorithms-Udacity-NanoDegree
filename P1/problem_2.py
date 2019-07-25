020eb668f2cf2b87f7c22d2bcc7f858f5e8c2f9d
Robert Walters <waltersmac@gmail.com>
2019-07-25 22:32:51 +0100

    feat: Updated problem2 with recusion code
    recusion code implemented, but unable to return list of paths
    
    Project: P1

--- P1/problem_2.py
+++ P1/problem_2.py
@@ -1,46 +1,4 @@
-
-class Node(object):
-        
-    def __init__(self,value = None):
-        self.value = value
-        self.left = None
-        self.right = None
-        
-    def set_value(self,value):
-        self.value = value
-        
-    def get_value(self):
-        return self.value
-        
-    def set_left_child(self,left):
-        self.left = left
-        
-    def set_right_child(self, right):
-        self.right = right
-        
-    def get_left_child(self):
-        return self.left
-    
-    def get_right_child(self):
-        return self.right
-
-    def has_left_child(self):
-        return self.left != None
-    
-    def has_right_child(self):
-        return self.right != None
-    
-    
-class Tree():
-    def __init__(self, value=None):
-        self.root = Node(value)
-        
-    def get_root(self):
-        return self.root
-
-        
-
-
+import os
 
 def find_files(suffix, path):
     """
@@ -58,7 +16,26 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-
+    paths = []
     list_of_paths = []
 
-    return None
+    current_dir = os.listdir(path)
+    paths.append(path)
+    
+    for item in current_dir:
+        new_path = os.path.join(path,item)
+
+        print(new_path)
+
+        if os.path.isdir(new_path):
+            find_files(suffix, new_path)
+            paths.append(new_path)
+        else:
+            paths.append(new_path)
+    
+    return paths
+
+suffix = ".c"
+path = './testdir'
+
+print(find_files(suffix,path))
