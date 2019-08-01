c13cb024fcd8363a03a8380564b38b48b9e53099
Robert Walters <waltersmac@gmail.com>
2019-08-01 22:09:57 +0100

    feat: Added combine method to problem3
    Added method to build the tree for the huffman algorithm.
    This is the first versio of this method.
    
    Project: P1

--- P1/problem_3.py
+++ P1/problem_3.py
@@ -1,6 +1,4 @@
 import sys
-import heapq
-import os
 
 
 class Node:
@@ -12,12 +10,23 @@ class Node:
 
 
 
-class Tree (object):
+class Tree_nodes (object):
     def __init__(self, path):
         self.path = path
-        self.heap = []
-        self.codes = {}
-        self.reverse_mapping = {}
+        self.nodes = []
+        self.tree = []
+        self.new_node = []
+        self.new_nodes = []
+        self.pos = 0
+
+    
+
+    def merge_nodes(self):
+        while len(self.tree) > 1:
+            merged = Node(None, node1.freq + node2.freq)
+            merged.left = node1
+            merged.right = node2
+
 
 
 
@@ -32,17 +41,54 @@ def to_set(data):
         else:
             char_freq[letter] = 1
     
-    char_freq = [(v, k) for k, v in char_freq.items()]
+    char_freq = [[v, k] for k, v in char_freq.items()]
 
     return sorted(char_freq, reverse=True)
 
 
 
+huffman_tree = []
+
+def combine(nodes):
+
+    pos = 0
+    new_node = []
+
+    if len(nodes) > 1:
+        nodes.sort()
+
+        nodes[pos].append("0")
+        nodes[pos+1].append("1")
+
+        combined_node1 = (nodes[pos][0] + nodes[pos+1][0])
+        combined_node2 = (nodes[pos][1] + nodes[pos+1][1])
+
+        new_node.append(combined_node1)
+        new_node.append(combined_node2)
+
+        newnodes = []
+
+        newnodes.append(new_node)
+        newnodes = newnodes + nodes[2:]
+        nodes = newnodes
+
+        huffman_tree.append(nodes)
+
+        combine(nodes)
+
+    return huffman_tree
+
 
 def huffman_encoding(data):
 
-    return to_set(data)
-    
+    nodes = to_set(data)
+
+    huffman_tree.append(nodes)
+
+    return combine(nodes)
+
+
+
     
 
 
