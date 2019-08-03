c754362d4ac8c0bedcbf8297c8e9bc92c9c10f49
Robert Walters <waltersmac@gmail.com>
2019-08-03 22:46:06 +0100

    updating problem 3

--- P1/problem_3.py
+++ P1/problem_3.py
@@ -1,31 +1,40 @@
 import sys
 
-
-class Node:
-    def __init__(self, char, freq):
-        self.char = char
-        self.freq = freq
-        self.left = None
-        self.right = None
+huffman_tree = []
 
 
+def combine(nodes):
 
-class Tree_nodes (object):
-    def __init__(self, path):
-        self.path = path
-        self.nodes = []
-        self.tree = []
-        self.new_node = []
-        self.new_nodes = []
-        self.pos = 0
+    pos = 0
+    new_node = []
 
-    
+    if len(nodes) > 1:
+        nodes.sort()
+        print(nodes)
 
-    def merge_nodes(self):
-        while len(self.tree) > 1:
-            merged = Node(None, node1.freq + node2.freq)
-            merged.left = node1
-            merged.right = node2
+        nodes[pos].append("0")
+        nodes[pos+1].append("1")
+        print(nodes)
+
+        combined_node1 = (nodes[pos][0] + nodes[pos+1][0])
+        combined_node2 = (nodes[pos][1] + nodes[pos+1][1])
+        print(combined_node1)
+        print(combined_node2)
+
+        new_node.append(combined_node1)
+        new_node.append(combined_node2)
+        
+        newnodes = []
+
+        newnodes.append(new_node)
+        newnodes = newnodes + nodes[2:]
+        nodes = newnodes
+        
+        huffman_tree.append(nodes)
+        
+        #combine(nodes)
+
+    return huffman_tree
 
 
 
@@ -47,45 +56,12 @@ def to_set(data):
 
 
 
-huffman_tree = []
-
-def combine(nodes):
-
-    pos = 0
-    new_node = []
-
-    if len(nodes) > 1:
-        nodes.sort()
-
-        nodes[pos].append("0")
-        nodes[pos+1].append("1")
-
-        combined_node1 = (nodes[pos][0] + nodes[pos+1][0])
-        combined_node2 = (nodes[pos][1] + nodes[pos+1][1])
-
-        new_node.append(combined_node1)
-        new_node.append(combined_node2)
-
-        newnodes = []
-
-        newnodes.append(new_node)
-        newnodes = newnodes + nodes[2:]
-        nodes = newnodes
-
-        huffman_tree.append(nodes)
-
-        combine(nodes)
-
-    return huffman_tree
-
 
 def huffman_encoding(data):
 
     nodes = to_set(data)
 
-    huffman_tree.append(nodes)
-
-    return combine(nodes)
+    return sorted(combine(nodes), reverse=True)
 
 
 
@@ -103,7 +79,8 @@ if __name__ == "__main__":
     print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
     print ("The content of the data is: {}\n".format(a_great_sentence))
     
-    print(huffman_encoding(a_great_sentence))
+    huffman_encoding(a_great_sentence)
+    
     #encoded_data, tree = huffman_encoding(a_great_sentence)
 
     #print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
