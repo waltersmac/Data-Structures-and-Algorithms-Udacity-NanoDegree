f6a0b7d7e275ca3427a4aa546cecadd46b33362e
Robert Walters <waltersmac@gmail.com>
2019-08-15 21:20:09 +0100

    Feat: Create floored square root algorithm
    Completed first version of the algorithm.
    
    Project: P2

--- P2/Problem_1.py
+++ P2/Problem_1.py
@@ -9,8 +9,6 @@ If the given number is 27, the answer would be 5 because sqrt(5) = 5.196 whose f
 
 The expected time complexity is O(log(n))'''
 
-print(math.sqrt(25))
-
 def sqrt(number):
     """
     Calculate the floored square root of a number
@@ -21,15 +19,27 @@ def sqrt(number):
        int: Floored Square Root
     """
     
-    if number < 0:
-    	return "Cannot sqrt number"
-
     if number <= 1:
     	return number
 
+    divsor = 2
+    result = 0
+
+    while divsor != result:
+    	result = number // divsor
+    	if divsor == result:
+    		return result
+    	divsor += 1
+
+
+
+print ("Pass" if  (2 == sqrt(4)) else "Fail")
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
 print ("Pass" if  (4 == sqrt(16)) else "Fail")
 print ("Pass" if  (1 == sqrt(1)) else "Fail")
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
-print ("Pass" if  ("Cannot sqrt number" == sqrt(-2)) else "Fail")
+print ("Pass" if  (6 == sqrt(36)) else "Fail")
+print ("Pass" if  (6 == sqrt(38)) else "Fail")
+print ("Pass" if  (9 == sqrt(81)) else "Fail")
+print ("Pass" if  (12 == sqrt(150)) else "Fail")
