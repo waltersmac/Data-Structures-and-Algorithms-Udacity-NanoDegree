4ffb0a717fe9a44f5891a344e361aa54cdd42e16
waltersmac <waltersmac@gmail.com>
2019-08-22 15:03:36 +0100

    Feat: Added list permutation
    Added list permutation tool to test algorithm, wich was succesful.
    
    Project: P2

--- P2/Problem_3.py
+++ P2/Problem_3.py
@@ -1,3 +1,11 @@
+import copy
+import itertools
+import math
+
+def combinations(target,data):
+    pass
+
+
 def rearrange_digits(input_list):
     """
     Rearrange Array Elements so as to form two number such that their sum is maximum.
@@ -7,7 +15,33 @@ def rearrange_digits(input_list):
     Returns:
        (int),(int): Two maximum sums
     """
-    pass
+
+    max_sum = 0
+    max_values = []
+
+    for l in list(itertools.permutations(input_list)):
+
+        if len(l) % 2 == 0:
+            midpoint = (len(l)-2) // 2
+        else:
+            midpoint = (len(l)-1) // 2
+
+    
+        value1 = int("".join(str(i) for i in l[:midpoint]))
+        value2 = int("".join(str(i) for i in l[midpoint:]))
+
+        value_sum = value1 + value2
+
+        if max_sum <= value_sum:
+            max_value2, max_value1  = value2, value1
+            max_sum = value_sum
+        
+    return max_value2, max_value1
+
+    
+    
+
+
 
 def test_function(test_case):
     output = rearrange_digits(test_case[0])
@@ -17,5 +51,7 @@ def test_function(test_case):
     else:
         print("Fail")
 
+
 test_function([[1, 2, 3, 4, 5], [542, 31]])
 test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+
