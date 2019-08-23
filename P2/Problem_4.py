ce4c0259e549ef6186d7b29ac0b58a1ff5be735d
waltersmac <waltersmac@gmail.com>
2019-08-23 20:14:42 +0100

    Feat: Completed Problem 4
    Completed the sort_012 function - The algorithm sorted that array by 0,1 and 2's
    
    Project: P2

--- P2/Problem_4.py
+++ P2/Problem_4.py
@@ -5,7 +5,30 @@ def sort_012(input_list):
     Args:
        input_list(list): List to be sorted
     """
-    pass
+
+    next_pos_0 = 0
+    next_pos_2 = len(input_list) - 1
+
+    front_index = 0
+
+    while front_index <= next_pos_2:
+
+        if input_list[front_index] == 0:
+            input_list[front_index] = input_list[next_pos_0]
+            input_list[next_pos_0] = 0
+            next_pos_0 += 1
+            front_index += 1
+
+        elif input_list[front_index] == 2:           
+            input_list[front_index] = input_list[next_pos_2] 
+            input_list[next_pos_2] = 2
+            next_pos_2 -= 1
+
+        else:
+            front_index += 1
+    
+    return input_list
+
 
 def test_function(test_case):
     sorted_array = sort_012(test_case)
@@ -14,6 +37,7 @@ def test_function(test_case):
         print("Pass")
     else:
         print("Fail")
+
 
 test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
 test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
