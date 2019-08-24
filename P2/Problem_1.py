9081d5b9a9f73774d03cd2cc3b7eab97f5232267
waltersmac <waltersmac@gmail.com>
2019-08-24 20:57:53 +0100

    general tidy up

--- P2/Problem_1.py
+++ P2/Problem_1.py
@@ -1,5 +1,3 @@
-import math
-
 '''Finding the Square Root of an Integer
 Find the square root of the integer without using any Python library. You have to find the floor value of the square root.
 
@@ -8,6 +6,8 @@ For example if the given number is 16, then the answer would be 4.
 If the given number is 27, the answer would be 5 because sqrt(5) = 5.196 whose floor value is 5.
 
 The expected time complexity is O(log(n))'''
+
+
 
 def sqrt(number):
     """
