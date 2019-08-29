e4aa75da78e73330987271860cf248859b7ec169
Robert Walters <waltersmac@gmail.com>
2019-08-29 22:52:26 +0100

    Feat: Added merge sort to Problem 3
    Implemented a merge sort into the alogorithm, which enabled me to arrange the
    values to create the maximum sum.
    
    Project: P2

--- P2/Problem_3.py
+++ P2/Problem_3.py
@@ -1,9 +1,36 @@
-import copy
-import itertools
-import math
+def mergesort(array):
+    if len(array) <= 1:
+        return array
 
-def combinations(target,data):
-    pass
+    if len(array) % 2 == 0:
+        midpoint = (len(array) // 2)
+    else:
+        midpoint = (len(array)+1) // 2
+
+    left = mergesort(array[:midpoint])
+    right = mergesort(array[midpoint:])
+
+    # Merge our two halves and return
+    return merge(left, right)
+
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+    
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(right[right_index])
+            right_index += 1
+        else:
+            merged.append(left[left_index])
+            left_index += 1
+
+    merged += left[left_index:]
+    merged += right[right_index:]
+        
+    return merged
 
 
 def rearrange_digits(input_list):
@@ -15,34 +42,23 @@ def rearrange_digits(input_list):
     Returns:
        (int),(int): Two maximum sums
     """
+    input_list = mergesort(input_list)
 
-    max_sum = 0
-    max_values = []
+    value1 = []
+    value2 = []
 
-    for l in list(itertools.permutations(input_list)):
-
-        if len(l) % 2 == 0:
-            midpoint = (len(l)-2) // 2
+    for i in range(len(input_list)):
+        if i % 2 == 0:
+            value1.append(input_list[i])
         else:
-            midpoint = (len(l)-1) // 2
+            value2.append(input_list[i])
 
-    
-        value1 = int("".join(str(i) for i in l[:midpoint]))
-        value2 = int("".join(str(i) for i in l[midpoint:]))
-
-        value_sum = value1 + value2
-
-        if max_sum <= value_sum:
-            max_value2, max_value1  = value2, value1
-            max_sum = value_sum
+    value1 = int("".join(str(i) for i in value1))
+    value2 = int("".join(str(i) for i in value2))
         
-    return max_value2, max_value1
-
+    return [value1, value2]
     
     
-
-
-
 def test_function(test_case):
     output = rearrange_digits(test_case[0])
     solution = test_case[1]
@@ -52,6 +68,7 @@ def test_function(test_case):
         print("Fail")
 
 
-test_function([[1, 2, 3, 4, 5], [542, 31]])
-test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
-
+test_case1 = test_function([[1, 2, 3, 4, 5], [542, 31]])
+test_case3 = test_function([[4, 6, 2, 5, 9, 8], [964, 852]])
+test_case2 = test_function([[1, 0], [0,1]])
+test_case2 = test_function([[6, 8, 3], [86, 3]])
