f03a3b8f977a3450b3de83bc708ea1bee537a37a
Robert Walters <waltersmac@gmail.com>
2019-08-31 14:52:35 +0100

    Feat: Completed min/max function
    created a min/max function that returns the values as a tuple.
    
    Project: P2

--- P2/Problem_6.py
+++ P2/Problem_6.py
@@ -5,7 +5,6 @@ def get_min_max(ints):
     Args:
        ints(list): list of integers containing one or more integers
     """
-
     min_num = ints[0]
     max_num = ints[0]
 
@@ -29,8 +28,11 @@ import random
 
 l = [i for i in range(0, 11)]  # a list containing 0 - 9
 random.shuffle(l)
+j = [i for i in range(0, 100)]  # a list containing 0 - 9
+random.shuffle(l)
+k = [i for i in range(0, 50)]  # a list containing 0 - 9
+random.shuffle(l)
 
 print ("Pass" if ((min(l), max(l)) == get_min_max(l)) else "Fail")
-
-print(l)
-print(get_min_max(l))
+print ("Pass" if ((min(j), max(j)) == get_min_max(j)) else "Fail")
+print ("Pass" if ((min(k), max(k)) == get_min_max(k)) else "Fail")
