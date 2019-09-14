8b73ac3d270cf7cedc08223966c85bc296d4866b
waltersmac <waltersmac@gmail.com>
2019-09-14 12:32:04 +0100

    Feat: Completed the A start alorithm
    Implemented the A start alogrithm.
    
    Project: P3

--- P3/aStar.py
+++ P3/aStar.py
@@ -1,21 +1,6 @@
 import M
 import math
-
-class Node():
-    """A node class for A* Pathfinding"""
-
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-
-        self.g = 0
-        self.h = 0
-        self.f = 0
-
-    def __eq__(self, other):
-        return self.position == other.position
-    
-       
+from queue import PriorityQueue       
 
 def shortest_path(roads, intersections,start,goal):
     print("shortest path called")
@@ -23,101 +8,81 @@ def shortest_path(roads, intersections,start,goal):
     # Creating varible helpers
     start_cords = intersections[start]
     goal_cords = intersections[goal]
-    
-    print("start co-ords",(start_cords[0],start_cords[1]))
-    # Create start and end node
-    start_node = Node(start, (start_cords[0],start_cords[1]))
-    start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(goal, (goal_cords[0],goal_cords[1]))
-    end_node.g = end_node.h = end_node.f = 0
 
     # Initialize both open and closed list
-    open_list = []
-    closed_list = []
+    G = {}
+    F = {}
+ 
+    #Initialize starting values
+    G[start] = 0 
 
-    # Add the start node
-    open_list.append(start_node)
+    # Calculating heuristic at starting point
+    x = (start_cords[0] - goal_cords[0])**2
+    y = (start_cords[1] - goal_cords[1])**2
+    heuristic_start = math.sqrt(x + y)
+    F[start] = heuristic_start
     
-
-    #
-    path = [] 
+    # Initialize path of route
+    path = []
     
-    # 
-    while len(open_list) > 0:
-        
-        current = min(open_list, key=lambda o:o.g + o.h)
-        print(current)
+    # Initialize sets to check nodes
+    open_set = set()
+    open_set.add(start)
+    closed_set = set()
+    came_from = {}
 
-        # Get the current node
-        current_node = open_list[0]
-        print(current_node.parent)
-        current_index = 0
-        for index, item in enumerate(open_list):
+    while len(open_set) > 0:
+
+        #Get the node in the open list with the lowest F score
+        current = None
+        current_f_score = None
+
+        for node in open_set:
+            if current is None or F[node] < current_f_score:
+                current_f_score = F[node]
+                current = node
+ 
+        #Check if we have reached the goal
+        if current == goal:
+            #Retrace our route backward
+            path = [current]
+            while current in came_from:
+                current = came_from[current]
+                path.append(current)
+            path.reverse()
+            return path #Done!
+
+        #Mark the current vertex as closed
+        open_set.remove(current)
+        closed_set.add(current)
             
-            if item.f < current_node.f or item.f == 0:
-            	current_node = item
-            	current_index = index       
+        for next in roads[current]:
 
-                
-
-        # Pop current off open list, add to closed list
-        open_list.pop(current_index)
-        closed_list.append(current_node)
-
-        
-        if current_node.parent == end_node.parent:
-        	path.append(current_node.parent)
-        	return path
-
-        else:
-        	path.append(current_node.parent)
-            
-
-        
-        # Generate children
-        children = []
-        
-        for new_position in roads[current_node.parent]:
-
-            # Get node position
-            node_position = (intersections[new_position][0], intersections[new_position][1])
-
-            # Create new node
-            new_node = Node(new_position, node_position)
-
-            # Append
-            children.append(new_node)
-
-            
-        # Loop through children
-        for child in children:
-
-
-            # Child is on the closed list
-            if child in closed_list:
+            if next in closed_set: 
                 continue
 
-            #print("current pos",current_node.parent,current_node.position[0],current_node.position[1],"child pos", child.position[0],child.position[1])
-            # Create the f, g, and h values
-            #print(((child.position[0] - current_node.position[0]) ** 2) + ((child.position[1] - current_node.position[1]) ** 2))
-            if child in open_list:
+            candidateG = G[current] + + math.sqrt(((intersections[next][0] - intersections[current][0]) ** 2) + ((intersections[next][1] - intersections[current][1]) ** 2))
+ 
+            if next not in open_set:
+                open_set.add(next) #Discovered a new vertex
+            elif candidateG >= G[next]:
+                continue #This G score is worse than previously found
+ 
+            #Adopt this G score
+            came_from[next] = current
+            G[next] = candidateG
 
-                child.g = current_node.g + math.sqrt(((child.position[0] - current_node.position[0])**2) + ((child.position[1] - current_node.position[1])**2))
-            
-                xVal = ((child.position[0] - end_node.position[0])**2)
-                yVal = ((child.position[1] - end_node.position[1])**2)
-                child.h = math.sqrt(xVal + yVal)
+            xVal = (intersections[next][0] - intersections[goal][0])**2
+            yVal = (intersections[next][1] - intersections[goal][1])**2
 
-                child.f = child.g + child.h
+            heuristic = math.sqrt(xVal + yVal)
 
-            print("child g", child.g, "child h", child.h,"child f", child.f)
-            
+            H = heuristic
+            F[next] = G[next] + H
 
-            # Add the child to the open list
-            open_list.append(child)
+
         
-        
-
+#print(shortest_path(M.roads,M.intersections, 8, 24))
 path = shortest_path(M.roads,M.intersections, 8, 24)
 if path == [8, 14, 16, 37, 12, 17, 10, 24]:
     print("great! Your code works for these inputs!")
